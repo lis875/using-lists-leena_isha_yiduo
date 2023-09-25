ab69372a49dfea5341a8bc8ffdc7181812970757
Leena <leena@wisc.edu>
2023-09-25 13:46:40 -0400

    Added the square program

---
+++ square.py
@@ -0,0 +1,9 @@
+#num_input=input("Enter the number of * you want to print")
+sides_input=input("Enter the number of lines you want to print")
+
+for count in range(0,int(sides_input)):
+    for i in range(0,int(sides_input)):
+        print("*", end="")
+    
+    print("")
+  
