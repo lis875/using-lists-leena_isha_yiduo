f0f4795b2b785c4e90ff1500757b55666a21c4c1
Isha-Sriv <isrivastava4@wisc.edu>
2023-09-25 14:15:20 -0500

    EX 4

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+l=input("Enter length of rectangle")
+w=input("Enter width of rectangle")
+
+for i in range (0, int(l)):
+    for j in range (0, int(w)):
+        print("*",end="")
+    print("")    
+    
