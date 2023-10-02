5b25e49da4e183c3fd253f4e9057f15388f33646
Isha-Sriv <isrivastava4@wisc.edu>
2023-10-02 00:43:32 -0500

    EX 11 & 12

--- coin.py
+++ coin.py
@@ -1,7 +1,14 @@
 import random
 
-num = random.randint(1,2)
-if (num == 1):
-  print("Heads")
-else:
-  print("Tails")
+head=0
+
+for i in range(20):
+    num = random.randint(1,2)
+    if (num == 1):
+        print("Heads")
+        head+=1
+    else:
+        print("Tails")
+        
+print("Number of heads ", head)
+print("Number of tails ", (20-head))
