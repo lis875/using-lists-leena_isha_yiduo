149e978db254cf77846c16bb77d29039534ac7d1
Leena <leena@wisc.edu>
2023-10-02 00:17:44 -0500

    Exercise5&6

--- names.py
+++ names.py
@@ -1,3 +1,15 @@
 names = ['Rick Wash', 'Rushali Bhudeka', 'Mickey Chen', 'Bhavya Shekhawat']
 
 print(names)
+
+# print after 1st append
+names.append("Leena Leena")
+print(names)
+
+# print after 2nd append
+names.append("Isha Srivastava")
+print(names)
+
+# print after 3rd append
+names.append("Yiduo")
+print(names)
