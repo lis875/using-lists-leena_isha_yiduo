f847faec8c5c8a1960a82fb50503ca19fe195b12
Leena <leena@wisc.edu>
2023-10-02 01:05:00 -0500

    Exercise 16

--- restaurant.py
+++ restaurant.py
@@ -2,9 +2,12 @@ import random
 
 restaurants = []
 
-for i in range(3):
+res_num = input("How many places are you thinking about for lunch?\n")
+res_num = int(res_num)
+
+for i in range(res_num):
     place = input("Enter the name of restuarant " + str(i+1) + ": ")
     restaurants.append(place)
 
-result = int(random.randint(1,3))
-print(restaurants[result])
+result = int(random.randint(1,res_num))
+print("You should go to", restaurants[result])
