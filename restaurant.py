0f5ea31a79698a7b0df19a41cd7eee67d5fd43cb
Isha-Sriv <isrivastava4@wisc.edu>
2023-10-02 00:52:39 -0500

    EX 14 & 15

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,10 @@
+import random
+
 restaurants = []
+
 for i in range(3):
-    place = input("Enter the name of restuarant " + str(i) + ": ")
+    place = input("Enter the name of restuarant " + str(i+1) + ": ")
     restaurants.append(place)
 
-print(restaurants)
+result = int(random.randint(1,3))
+print(restaurants[result])
